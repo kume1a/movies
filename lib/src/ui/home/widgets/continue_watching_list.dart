c10e5dbe062d0612820c75e9849c2b3241cc5084
kume1a <kumela011@gmail.com>
2021-07-06 17:24:27 +0400

    fixed continue watching list not refreshing

--- lib/src/ui/home/widgets/continue_watching_list.dart
+++ lib/src/ui/home/widgets/continue_watching_list.dart
@@ -24,89 +24,93 @@ class ContinueWatchingList extends StatelessWidget {
     return BlocBuilder<HomeBloc, HomeState>(
       buildWhen: (HomeState prev, HomeState curr) =>
           !const DeepCollectionEquality().equals(prev.savedMovies, curr.savedMovies),
-      builder: (BuildContext context, HomeState state) =>
-          state.savedMovies?.isNotEmpty == true ? _buildList(context, state.savedMovies!) : const SizedBox.shrink(),
+      builder: (BuildContext context, HomeState state) => _buildList(context, state.savedMovies),
     );
   }
 
-  Widget _buildList(BuildContext context, List<SavedMovie> savedMovies) {
+  Widget _buildList(BuildContext context, List<SavedMovie>? savedMovies) {
     return VisibilityDetector(
       key: UniqueKey(),
-      onVisibilityChanged: (VisibilityInfo info) =>
-          context.read<HomeBloc>().add(const HomeEvent.refreshSavedMoviesRequested()),
-      child: SizedBox(
-        height: itemHeight,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: savedMovies.length,
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          itemBuilder: (BuildContext context, int index) {
-            final SavedMovie savedMovie = savedMovies[index];
-
-            String bottomDetailText = formatVideoDuration(savedMovie.position.leftAt);
-            if (savedMovie.position.isTvShow) {
-              final int s = savedMovie.position.season;
-              final int e = savedMovie.position.episode;
-              bottomDetailText += '\t--\t${s}x${e < 10 ? '0$e' : e}';
-            }
-
-            final double progress = savedMovie.position.durationInMillis != 0
-                ? savedMovie.position.leftAt / savedMovie.position.durationInMillis
-                : 0;
-
-            return GestureDetector(
-              onTap: () => ScreensNavigator.pushStreamPage(
-                movieId: savedMovie.position.movieId,
-                season: savedMovie.position.season,
-                episode: savedMovie.position.episode,
-                leftAt: savedMovie.position.leftAt,
-              ),
-              child: Padding(
+      onVisibilityChanged: (VisibilityInfo info) {
+        if (info.visibleFraction == 1) {
+          context.read<HomeBloc>().add(const HomeEvent.refreshSavedMoviesRequested());
+        }
+      },
+      child: savedMovies?.isNotEmpty == true
+          ? SizedBox(
+              height: itemHeight,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: savedMovies!.length,
                 padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(radius),
-                      child: Stack(
+                itemBuilder: (BuildContext context, int index) {
+                  final SavedMovie savedMovie = savedMovies[index];
+
+                  String bottomDetailText = formatVideoDuration(savedMovie.position.leftAt);
+                  if (savedMovie.position.isTvShow) {
+                    final int s = savedMovie.position.season;
+                    final int e = savedMovie.position.episode;
+                    bottomDetailText += '\t--\t${s}x${e < 10 ? '0$e' : e}';
+                  }
+
+                  final double progress = savedMovie.position.durationInMillis != 0
+                      ? savedMovie.position.leftAt / savedMovie.position.durationInMillis
+                      : 0;
+
+                  return GestureDetector(
+                    onTap: () => ScreensNavigator.pushStreamPage(
+                      movieId: savedMovie.position.movieId,
+                      season: savedMovie.position.season,
+                      episode: savedMovie.position.episode,
+                      leftAt: savedMovie.position.leftAt,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          SafeImage(
-                            imageUrl: savedMovie.data.availableImage,
-                            width: itemWidth,
-                            height: imageHeight,
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            child: SizedBox(
-                              width: itemWidth,
-                              child: LinearProgressIndicator(
-                                backgroundColor: colorInactive,
-                                value: progress,
-                              ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(radius),
+                            child: Stack(
+                              children: <Widget>[
+                                SafeImage(
+                                  imageUrl: savedMovie.data.availableImage,
+                                  width: itemWidth,
+                                  height: imageHeight,
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  child: SizedBox(
+                                    width: itemWidth,
+                                    child: LinearProgressIndicator(
+                                      backgroundColor: colorInactive,
+                                      value: progress,
+                                    ),
+                                  ),
+                                )
+                              ],
                             ),
-                          )
+                          ),
+                          const SizedBox(height: 6),
+                          SizedBox(
+                            width: itemWidth,
+                            child: Text(
+                              savedMovie.data.name,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: prSB18,
+                            ),
+                          ),
+                          const SizedBox(height: 2),
+                          Text('left at $bottomDetailText', style: sc11),
                         ],
                       ),
                     ),
-                    const SizedBox(height: 6),
-                    SizedBox(
-                      width: itemWidth,
-                      child: Text(
-                        savedMovie.data.name,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: prSB18,
-                      ),
-                    ),
-                    const SizedBox(height: 2),
-                    Text('left at $bottomDetailText', style: sc11),
-                  ],
-                ),
+                  );
+                },
               ),
-            );
-          },
-        ),
-      ),
+            )
+          : const SizedBox(width: 1, height: 1),
     );
   }
 }
