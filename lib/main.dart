8ce994d49b69d543d5ca840cd2fb4836a1eb1f35
kume1a <kumela011@gmail.com>
2021-09-26 12:25:01 +0400

    patched bad certificate issue on some devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,8 +10,18 @@ import 'src/ui/app.dart';
 
 // TODO: add subtitles from api and allow to import, group name edit. blurhash, download video
 
+class SafeCertificateHttpOverride extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  HttpOverrides.global = SafeCertificateHttpOverride();
 
   await configureInjection(Environment.prod);
   await SystemChrome.setPreferredOrientations(<DeviceOrientation>[DeviceOrientation.portraitUp]);
